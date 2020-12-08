18db684ab873792d47bf12790cf3e7e8b50e9abe
mohanson <mohanson@outlook.com>
2020-12-09 00:16:15 +0800

    2020/12/09

--- make.py
+++ make.py
@@ -4,7 +4,7 @@ import sys
 
 
 def call(command):
-    print(command)
+    print('$', command)
     r = subprocess.call(command, shell=True)
     if r != 0:
         sys.exit(r)
@@ -15,14 +15,9 @@ def make():
     call('mkdocs build -d site_build')
     call('rm -rf site')
     call('mv site_build site')
-
-    with open('site/baidu_verify_Pem1L7uAVI.html', 'w') as f:
-        f.write('Pem1L7uAVI')
-    with open('site/google9b75b4b4147e247b.html', 'w') as f:
-        f.write('google-site-verification: google9b75b4b4147e247b.html')
-    with open('site/ads.txt', 'w') as f:
-        f.write('google.com, pub-5236818090688638, DIRECT, f08c47fec0942fa0')
-
+    call('echo -n "Pem1L7uAVI" > site/baidu_verify_Pem1L7uAVI.html')
+    call('echo -n "google-site-verification: google9b75b4b4147e247b.html" > site/google9b75b4b4147e247b.html')
+    call('echo -n "google.com, pub-5236818090688638, DIRECT, f08c47fec0942fa0" > site/ads.txt')
     call('rm site/img/favicon.ico')
     call('cp docs/img/favicon.ico site/img/favicon.ico')
 
