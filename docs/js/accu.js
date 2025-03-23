9f8175556227efe7a1c8826a3fa693faefb3f1a0
mohanson <mohanson@outlook.com>
2025-03-23 20:57:16 +0800

    2025-03-23 20:57:16

--- docs/js/accu.js
+++ docs/js/accu.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     SphinxRtdTheme.Navigation.onScroll = function () { }
     setTimeout(function () {
+        if (window.location.pathname != '/') {
+            $('.wy-side-scroll').scrollTop($('.wy-side-scroll').scrollTop() - 60);
+        }
         $('.wy-side-scroll').css('opacity', 1.0);
     }, 16.66)
 })
